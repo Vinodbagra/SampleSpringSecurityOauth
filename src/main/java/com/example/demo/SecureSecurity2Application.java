76f4756dc66f61de0e47baba5d6985c59e00aea0
VinodSharma1 <vinodh@fatcat.net.in>
2022-10-28 17:42:49 +0530

    oauth2

---
+++ src/main/java/com/example/demo/SecureSecurity2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureSecurity2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureSecurity2Application.class, args);
+	}
+
+}
